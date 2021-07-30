f9c71ec627ba7b112120bbe24872665f42db3ffa
TylerLadas <tylerladas@hotmail.com>
2021-07-30 10:52:08 -0400

    add router folders and link to server.js

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,42 +1,44 @@
 // Dependencies
 // =============================================================
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 // API Routes
 // =============================================================
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     //creates variable from db.json file
-    let notes = JSON.parse(fs.readFileSync('../../data/db.json', 'utf8'));
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
     //return notes to client
     return res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     let newNote = req.body;
     //generate unique id with uuid package
     newNote.id = uuidv4();
     //create variable from db.json file
-    let notes = JSON.parse(fs.readFileSync('../../data/db.json', 'utf8'));
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
     //push newNote to notes variable 
     notes.push(newNote);
     //write notes variable to db.json file
-    fs.writeFileSync('../../data/db.json', JSON.stringify(notes));
+    fs.writeFileSync('./data/db.json', JSON.stringify(notes));
     //return notes to client
     return res.json(notes);
 });
 
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     //creates variable from file to delete
     let deleteNote = req.params.id;
     //creates variable from db.json file
-    let notes = JSON.parse(fs.readFileSync('../../data/db.json', 'utf8'));
-    //filters element by id from notes notes and creates newNotes array 
-    let newNotes = notes.filter(note => note.id !== deleteNote);
-    //write newNotes array to db.json file
-    fs.writeFileSync('../../data/db.json', JSON.stringify(newNotes));
-    //return newNotes to client
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
+    //removes/filters deleteFile from notes
+    let newNotes = notes.filter(file => file.id !== deleteNote);
+    //write notes variable to db.json file
+    fs.writeFileSync('./data/db.json', JSON.stringify(newNotes));
+    //return notes to client
     return res.json(newNotes);
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 // Dependencies
 // =============================================================
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
 
+// instantiate server
 const app = express();
-const PORT = 3001;
+
+// create port
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsinng
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// access to router folders
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
 // public folder middleware for access to public folder CSS and JS
 app.use(express.static('public'));
+
+// use route folders
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Listener
 // =============================================================
